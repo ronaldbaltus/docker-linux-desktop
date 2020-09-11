93ab1f1797d41e9e46c6c5b1f99234dcaef0fd38
Ronald Baltus <elquedro@gmail.com>
2020-09-11 20:44:10 +0200

    Added user to the sudoers file

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ service dbus start
 # Add desktop user
 echo "Provisioning user ${Username}"
 useradd -ms /bin/bash ${Username}
+usermod -aG sudo ${Username}
 echo ${Username}:${Username} | chpasswd
 echo root:${Username} | chpasswd
 
